816c4e75e03f8ac079d0ca8dc69f50d551ac082f
aplatov <aplatov>
2007-04-04 23:08:57 +0000

    small fix

--- javascript/plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/wizards/JavascriptProjectCreationWizard.java
+++ javascript/plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/wizards/JavascriptProjectCreationWizard.java
@@ -68,7 +68,7 @@ public class JavascriptProjectCreationWizard extends NewElementWizard implements
 				fInterpreterGroup = new JavascriptInterpreterGroup(parent);
 			}
 
-			protected Observer getInterpreterGroupObservable() {
+			protected Observable getInterpreterGroupObservable() {
 				return fInterpreterGroup;
 			}
 
