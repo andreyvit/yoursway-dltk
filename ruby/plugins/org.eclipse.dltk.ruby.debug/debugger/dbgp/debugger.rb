3999c7543632bbbba7b6ad11137f04f97d5f0f95
dkovalev <dkovalev>
2007-05-13 18:11:24 +0000

    Removed unused files from debugger

--- ruby/plugins/org.eclipse.dltk.ruby.debug/debugger/dbgp/debugger.rb
+++ ruby/plugins/org.eclipse.dltk.ruby.debug/debugger/dbgp/debugger.rb
@@ -138,57 +138,53 @@ module XoredDebugger
 
 
     class Starter
+        def Starter.go
+            log = ENV['DBGP_RUBY_LOG']
+            logger = log.nil? ? NullLogger.new : (log == 'stdout' ? StdoutLogger.new : FileLogger.new(log))
 
-    def Starter.go
+            host   = ENV['DBGP_RUBY_HOST']
+            port   = ENV['DBGP_RUBY_PORT'].to_i
+            key    = ENV['DBGP_RUBY_KEY']
+            script = ENV['DBGP_RUBY_SCRIPT']
+            test   = ENV['DBGP_RUBY_TEST']
+            test   = test.nil? ? false : test == '1' ? true : false
 
-    log = ENV['DBGP_RUBY_LOG']
-    logger = log.nil? ? NullLogger.new : (log == 'stdout' ? StdoutLogger.new : FileLogger.new(log))
+            begin
+                if (host.nil? or port == 0 or key.nil? or script.nil?)
+                    logger.puts('Invalid debugger params')
+                else
+                    logger.puts('Debugging session on ' + Time.new.to_s)
+                    logger.puts('Host: ' + host.to_s)
+                    logger.puts('Port: ' + port.to_s)
+                    logger.puts('Key: ' + key.to_s)
+                    logger.puts('Script: ' + script.to_s)
+                    logger.puts('Test: ' + test.to_s)
 
-    host   = ENV['DBGP_RUBY_HOST']
-    port   = ENV['DBGP_RUBY_PORT'].to_i
-    key    = ENV['DBGP_RUBY_KEY']
-    script = ENV['DBGP_RUBY_SCRIPT']
-    test   = ENV['DBGP_RUBY_TEST']
-    test   = test.nil? ? false : test == '1' ? true : false
-
-    begin
-        if (host.nil? or port == 0 or key.nil? or script.nil?)
-            logger.puts('Invalid debugger params')
-        else
-            logger.puts('Debugging session on ' + Time.new.to_s)
-            logger.puts('Host: ' + host.to_s)
-            logger.puts('Port: ' + port.to_s)
-            logger.puts('Key: ' + key.to_s)
-            logger.puts('Script: ' + script.to_s)
-            logger.puts('Test: ' + test.to_s)
-
-            # Debugger setup
-            @@debugger = MyDebugger.new(host, port, key, logger, test)
+                    # Debugger setup
+                    @@debugger = MyDebugger.new(host, port, key, logger, test)
             
-            set_trace_func proc { |event, file, line, id, binding, klass, *rest|
-                @@debugger.trace(event, file, line, id, binding, klass)
-            }
+                    set_trace_func proc { |event, file, line, id, binding, klass, *rest|
+                        @@debugger.trace(event, file, line, id, binding, klass)
+                    }
 
-            load script
+                    load script
 
-            set_trace_func nil
-            @@debugger.terminate
-         end
+                    set_trace_func nil
+                    @@debugger.terminate
+                end
 
-    rescue Exception
-        logger.puts('Exception during debugging:')
-        logger.puts('Message:')
-        logger.puts($!.message)
-        logger.puts('Backtrace:')
-        logger.puts($!.backtrace.join("\n"))
-    ensure
-        logger.close
-    end
-
-    end # go
+            rescue Exception
+                logger.puts('Exception during debugging:')
+                logger.puts('Message:')
+                logger.puts($!.message)
+                logger.puts('Backtrace:')
+                logger.puts($!.backtrace.join("\n"))
+            ensure
+                logger.close
+            end  
+        end # go
+    end # class Starter
 
     Starter.go
-
-    end # class Starter
 
 end # module
