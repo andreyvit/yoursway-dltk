e7a641fc3c29d2d39e4e50d2b53e8de7e7ddf736
ppetroche <ppetroche>
2007-06-28 10:23:56 +0000

    runner extensibility fixes

--- javascript/plugins/org.eclipse.dltk.javascript.launching/src/org/eclipse/dltk/javascript/launching/IJavaScriptInterpreterRunnerConfig.java
+++ javascript/plugins/org.eclipse.dltk.javascript.launching/src/org/eclipse/dltk/javascript/launching/IJavaScriptInterpreterRunnerConfig.java
@@ -7,12 +7,42 @@ import org.eclipse.jdt.launching.VMRunnerConfiguration;
 
 public interface IJavaScriptInterpreterRunnerConfig {
 
+	/**
+	 * 
+	 * @param config
+	 * @param launch
+	 * @param project
+	 * @return full name of class that runs javascript file and participates in initiating debugging session.
+	 */
 	public String getRunnerClassName(InterpreterConfig config, ILaunch launch,IJavaProject project);
 
+	/**
+	 * 
+	 * @param config
+	 * @param launch
+	 * @param project
+	 * @return computes class path for given arguments 
+	 * @throws Exception
+	 */
 	public String[] computeClassPath(InterpreterConfig config, ILaunch launch,IJavaProject project) throws Exception;
 
+	/**
+	 * resolves program arguments
+	 * @param config
+	 * @param launch
+	 * @param project
+	 * @return
+	 */
 	public String[] getProgramArguments(InterpreterConfig config, ILaunch launch,IJavaProject project);
 
+	
+	/**
+	 * any extra configuration of VMRunnerConfiguration can be done here
+	 * @param vconfig
+	 * @param iconfig
+	 * @param launch
+	 * @param project
+	 */
 	public void adjustRunnerConfiguration(VMRunnerConfiguration vconfig,
 			InterpreterConfig iconfig, ILaunch launch,IJavaProject project);
 }
