bff28a30814359ea0b49fcfe5b0c39d841b82678
aplatov <aplatov>
2007-07-31 12:45:03 +0000

    AllTests.java

--- javascript/tests/org.eclipse.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/AllTests.java
+++ javascript/tests/org.eclipse.javascript.core.tests/src/org/eclipse/dltk/javascript/core/tests/AllTests.java
@@ -9,7 +9,7 @@ public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite(
-				"Test for org.eclipse.dltk.javascript.core.tests.contentassist");
+				"Test for org.eclipse.dltk.javascript.core");
 		//$JUnit-BEGIN$
 		suite.addTestSuite(CodeCompletion.class);
 		//$JUnit-END$
