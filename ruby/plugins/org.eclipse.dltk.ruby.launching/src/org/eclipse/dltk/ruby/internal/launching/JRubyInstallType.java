c8d2a6f8b157e3976da5cd55c4236e4699329679
dkovalev <dkovalev>
2007-08-07 08:21:35 +0000

    Additional check for jruby interpreter name during search on non-Windows platform

--- ruby/plugins/org.eclipse.dltk.ruby.launching/src/org/eclipse/dltk/ruby/internal/launching/JRubyInstallType.java
+++ ruby/plugins/org.eclipse.dltk.ruby.launching/src/org/eclipse/dltk/ruby/internal/launching/JRubyInstallType.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.IOException;
 
 import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.dltk.internal.launching.AbstractInterpreterInstallType;
+import org.eclipse.dltk.internal.launching.InterpreterMessages;
 import org.eclipse.dltk.launching.IInterpreterInstall;
 import org.eclipse.dltk.ruby.core.RubyNature;
 import org.eclipse.dltk.ruby.launching.RubyLaunchingPlugin;
@@ -46,5 +49,18 @@ public class JRubyInstallType extends AbstractInterpreterInstallType {
 
 	protected ILog getLog() {
 		return RubyLaunchingPlugin.getDefault().getLog();
+	}
+
+	public IStatus validateInstallLocation(File installLocation) {
+		if (!Platform.getOS().equals(Platform.OS_WIN32)) {
+			if (installLocation.getName().indexOf(".bat") != 0) {
+				return createStatus(
+						IStatus.ERROR,
+						InterpreterMessages.errNonExistentOrInvalidInstallLocation,
+						null);
+			}
+		}
+
+		return super.validateInstallLocation(installLocation);
 	}
 }
