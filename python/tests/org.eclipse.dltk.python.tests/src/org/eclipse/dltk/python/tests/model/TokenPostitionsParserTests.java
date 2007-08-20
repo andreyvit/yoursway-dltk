f6c4f52ff0aa6f6a33b67a8975fec557b1364407
asobolev <asobolev>
2007-08-20 08:32:10 +0000

    * patch applied

--- python/tests/org.eclipse.dltk.python.tests/src/org/eclipse/dltk/python/tests/model/TokenPostitionsParserTests.java
+++ python/tests/org.eclipse.dltk.python.tests/src/org/eclipse/dltk/python/tests/model/TokenPostitionsParserTests.java
@@ -7,20 +7,26 @@ import junit.framework.Test;
 
 import org.eclipse.dltk.ast.ASTNode;
 import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
+import org.eclipse.dltk.ast.expressions.NumericLiteral;
 import org.eclipse.dltk.ast.statements.Block;
-import org.eclipse.dltk.ast.statements.Statement;
 import org.eclipse.dltk.core.tests.model.SuiteOfTestCases;
-import org.eclipse.dltk.core.tests.model.SuiteOfTestCases.Suite;
 import org.eclipse.dltk.python.internal.core.parser.PythonSourceParser;
 import org.eclipse.dltk.python.parser.ast.PythonWhileStatement;
 import org.eclipse.dltk.python.parser.ast.expressions.PrintExpression;
+import org.eclipse.dltk.python.parser.ast.expressions.PythonDictExpression;
+import org.eclipse.dltk.python.parser.ast.expressions.PythonListExpression;
 import org.eclipse.dltk.python.parser.ast.expressions.PythonTestListExpression;
+import org.eclipse.dltk.python.parser.ast.expressions.PythonTupleExpression;
 
 public class TokenPostitionsParserTests extends SuiteOfTestCases {
 
 	private static final String whileScript = "a=1; while a>0 : a=a-1;";
 	private static final String whileElseScript = "a=1; while a>0 : a=a-1; else : a = 1;";
 	private static final String testExprScript = "print \"Hello,\", \"World!\"";
+	private static final String testDictExprScript = "{ {   }:{} }";
+	private static final String testTupleExprScript  = "( 1, () )";
+	private static final String testListExprScript = "[ [],[] ]";
+	private static final String testBackQuotesScript  = "` 0 `";
 	
 	private static final String msg = "Invalid token displacement";
 
@@ -84,4 +90,41 @@ public class TokenPostitionsParserTests extends SuiteOfTestCases {
 		PythonTestListExpression testListExpr = (PythonTestListExpression) printExpr.getChilds().get(0);
 		assertTrue(msg, testListExpr.sourceEnd() > testListExpr.sourceStart() && testListExpr.sourceStart() > printExpr.sourceStart());
 	}
+	public void testDictExpression()
+	{
+		PythonSourceParser parser = new PythonSourceParser();
+		ModuleDeclaration module = parser.parse(null, testDictExprScript.toCharArray(), null);
+		List children = module.getChilds();
+
+		PythonDictExpression expr = (PythonDictExpression)((Block)children.get(0)).getChilds().get(0);
+		assertTrue(msg, expr.sourceStart() == 0 && expr.sourceEnd() == 12);
+	}
+	public void testTupleExpression()
+	{
+		PythonSourceParser parser = new PythonSourceParser();
+		ModuleDeclaration module = parser.parse(null, testTupleExprScript.toCharArray(), null);
+		List children = module.getChilds();
+
+		PythonTupleExpression expr = (PythonTupleExpression)((Block)children.get(0)).getChilds().get(0);
+		assertTrue(msg, expr.sourceStart() == 0 && expr.sourceEnd() == 9);
+	}
+	public void testListExpression()
+	{
+		PythonSourceParser parser = new PythonSourceParser();
+		ModuleDeclaration module = parser.parse(null, testListExprScript.toCharArray(), null);
+		List children = module.getChilds();
+
+		PythonListExpression expr = (PythonListExpression)((Block)children.get(0)).getChilds().get(0);
+		assertTrue(msg, expr.sourceStart() == 0 && expr.sourceEnd() == 9);
+	}
+	public void testBackQuotesExpression()
+	{
+		PythonSourceParser parser = new PythonSourceParser();
+		ModuleDeclaration module = parser.parse(null, testBackQuotesScript.toCharArray(), null);
+		List children = module.getChilds();
+
+		NumericLiteral expr = (NumericLiteral)((Block)children.get(0)).getChilds().get(0);
+		assertTrue(msg, expr.sourceStart() == 0 && expr.sourceEnd() == 5);
+	}
+	
 }
