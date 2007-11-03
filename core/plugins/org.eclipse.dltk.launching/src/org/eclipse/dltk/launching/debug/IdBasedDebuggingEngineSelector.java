adf443a42941c52d33c3d1241ea3b4d4496b3ba2
jgangemi <jgangemi>
2007-11-03 23:43:33 +0000

    - if there's only a single debugging engine, return it as the default

--- core/plugins/org.eclipse.dltk.launching/src/org/eclipse/dltk/launching/debug/IdBasedDebuggingEngineSelector.java
+++ core/plugins/org.eclipse.dltk.launching/src/org/eclipse/dltk/launching/debug/IdBasedDebuggingEngineSelector.java
@@ -4,8 +4,13 @@ public abstract class IdBasedDebuggingEngineSelector implements
 		IDebuggingEngineSelector {
 
 	public IDebuggingEngine select(IDebuggingEngine[] engines) {
+		// if there's only one engine, return it as the default
+		if (engines.length == 1) {
+			return engines[0];
+		}
+		
 		String id = getDebuggingEngineId();
-
+		
 		if (id != null) {
 			for (int i = 0; i < engines.length; ++i) {
 				IDebuggingEngine engine = engines[i];
