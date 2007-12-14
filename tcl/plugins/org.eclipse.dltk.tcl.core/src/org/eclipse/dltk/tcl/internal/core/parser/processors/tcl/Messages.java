6e1edb74a4cb5e3d034f088935cb5d7e841c39c7
asobolev <asobolev>
2007-12-14 12:33:34 +0000

    *fix

--- tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/core/parser/processors/tcl/Messages.java
+++ tcl/plugins/org.eclipse.dltk.tcl.core/src/org/eclipse/dltk/tcl/internal/core/parser/processors/tcl/Messages.java
@@ -3,7 +3,7 @@ package org.eclipse.dltk.tcl.internal.core.parser.processors.tcl;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.dltk.xotcl.internal.core.parser.processors.tcl.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.eclipse.dltk.tcl.internal.core.parser.processors.tcl.messages"; //$NON-NLS-1$
 	public static String TclProcProcessor_Empty_Proc_Name;
 	public static String TclProcProcessor_Wrong_Number_of_Arguments;
 	static {
