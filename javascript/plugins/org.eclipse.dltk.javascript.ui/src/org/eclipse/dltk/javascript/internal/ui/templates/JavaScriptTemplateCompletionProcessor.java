219a623b049d12ffed8b8bb40cf8b31332fd3d2b
jgangemi <jgangemi>
2008-03-11 01:58:43 +0000

    - revert back to using char[] for ignored chars

--- javascript/plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/templates/JavaScriptTemplateCompletionProcessor.java
+++ javascript/plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/templates/JavaScriptTemplateCompletionProcessor.java
@@ -9,70 +9,41 @@
  *******************************************************************************/
 package org.eclipse.dltk.javascript.internal.ui.templates;
 
-import org.eclipse.dltk.ui.DLTKPluginImages;
-import org.eclipse.dltk.ui.templates.ScriptTempalteCompletionProcessor;
+import org.eclipse.dltk.ui.templates.ScriptTemplateAccess;
+import org.eclipse.dltk.ui.templates.ScriptTemplateCompletionProcessor;
 import org.eclipse.dltk.ui.text.completion.ScriptContentAssistInvocationContext;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.templates.Template;
-import org.eclipse.jface.text.templates.TemplateContextType;
-import org.eclipse.swt.graphics.Image;
 
+/**
+ * JavaScript template completion processor
+ */
 public class JavaScriptTemplateCompletionProcessor extends
-		ScriptTempalteCompletionProcessor {
+		ScriptTemplateCompletionProcessor {
 
+	private static char[] IGNORE = new char[] {'.', ':', '@', '$' };
+	
 	public JavaScriptTemplateCompletionProcessor(
 			ScriptContentAssistInvocationContext context) {
 		super(context);
 	}
 
-	protected Template[] getTemplates(String contextTypeId) {
-		if (contextTypeId
-				.equals(JavaScriptUniversalTemplateContextType.CONTEXT_TYPE_ID)) {
-			return JavaScriptTemplateAccess.getInstance().getTemplateStore()
-					.getTemplates();
-		}
-
-		return new Template[0];
+	/*
+	 * @see org.eclipse.dltk.ui.templates.ScriptTemplateCompletionProcessor#getContextTypeId()
+	 */
+	protected String getContextTypeId() {
+		return JavaScriptUniversalTemplateContextType.CONTEXT_TYPE_ID;
 	}
 
-	protected TemplateContextType getContextType(ITextViewer viewer,
-			IRegion region) {
-
-		// Simple checking if completion string contains '.'
-		// TODO: make smarter
-		IDocument doc = viewer.getDocument();
-		try {
-			IRegion line = doc.getLineInformationOfOffset(region.getOffset()
-					+ region.getLength());
-			int len = region.getOffset() + region.getLength()
-					- line.getOffset();
-			String s = doc.get(line.getOffset(), len);
-
-			int spaceIndex = s.lastIndexOf(' ');
-			if (spaceIndex != -1) {
-				s = s.substring(spaceIndex);
-			}
-
-			if (s.indexOf('.') == -1 && s.indexOf(':') == -1
-					&& s.indexOf('@') == -1 && s.indexOf('$') == -1) {
-				return JavaScriptTemplateAccess
-						.getInstance()
-						.getContextTypeRegistry()
-						.getContextType(
-								JavaScriptUniversalTemplateContextType.CONTEXT_TYPE_ID);
-			}
-
-		} catch (BadLocationException e) {
-			e.printStackTrace();
-		}
-
-		return null;
+	/*
+	 * @see org.eclipse.dltk.ui.templates.ScriptTemplateCompletionProcessor#getIgnore()
+	 */
+	protected char[] getIgnore() {
+		return IGNORE;
 	}
-
-	protected Image getImage(Template template) {
-		return DLTKPluginImages.get(DLTKPluginImages.IMG_OBJS_TEMPLATE);
+	
+	/*
+	 * @see org.eclipse.dltk.ui.templates.ScriptTemplateCompletionProcessor#getTemplateAccess()
+	 */
+	protected ScriptTemplateAccess getTemplateAccess() {
+		return JavaScriptTemplateAccess.getInstance();
 	}
 }
--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/templates/TclTemplateCompletionProcessor.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/templates/TclTemplateCompletionProcessor.java
@@ -4,73 +4,44 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- 
- *******************************************************************************/
+ ******************************************************************************/
 package org.eclipse.dltk.tcl.internal.ui.templates;
 
-import org.eclipse.dltk.ui.templates.ScriptTempalteCompletionProcessor;
+import org.eclipse.dltk.ui.templates.ScriptTemplateAccess;
+import org.eclipse.dltk.ui.templates.ScriptTemplateCompletionProcessor;
 import org.eclipse.dltk.ui.text.completion.ScriptContentAssistInvocationContext;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.templates.Template;
-import org.eclipse.jface.text.templates.TemplateContextType;
-import org.eclipse.swt.graphics.Image;
 
+/**
+ * Tcl template completion processor
+ */
 public class TclTemplateCompletionProcessor extends
-		ScriptTempalteCompletionProcessor {
+		ScriptTemplateCompletionProcessor {
+	
+	private static char[] IGNORE = new char[] { '.' };
 
 	public TclTemplateCompletionProcessor(
 			ScriptContentAssistInvocationContext context) {
 		super(context);
 	}
 
-	protected Template[] getTemplates(String contextTypeId) {
-		if (contextTypeId
-				.equals(TclUniversalTemplateContextType.CONTEXT_TYPE_ID)) {
-			return TclTemplateAccess.getInstance().getTemplateStore()
-					.getTemplates();
-		}
-
-		return new Template[0];
+	/*
+	 * @see org.eclipse.dltk.ui.templates.ScriptTemplateCompletionProcessor#getContextTypeId()
+	 */
+	protected String getContextTypeId() {
+		return TclUniversalTemplateContextType.CONTEXT_TYPE_ID;
 	}
 
-	protected TemplateContextType getContextType(ITextViewer viewer,
-			IRegion region) {
-
-		// Simple checking if completion string contains '.'
-		// TODO: make smarter
-		IDocument doc = viewer.getDocument();
-		try {
-			IRegion line = doc.getLineInformationOfOffset(region.getOffset()
-					+ region.getLength());
-			int len = region.getOffset() + region.getLength()
-					- line.getOffset();
-			String s = doc.get(line.getOffset(), len);
-
-			int spaceIndex = s.lastIndexOf(' ');
-			if (spaceIndex != -1) {
-				s = s.substring(spaceIndex);
-			}
-
-			if (s.indexOf('.') == -1) {
-				return TclTemplateAccess
-						.getInstance()
-						.getContextTypeRegistry()
-						.getContextType(
-								TclUniversalTemplateContextType.CONTEXT_TYPE_ID);
-			}
-
-		} catch (BadLocationException e) {
-			e.printStackTrace();
-		}
-
-		return null;
+	/*
+	 * @see org.eclipse.dltk.ui.templates.ScriptTemplateCompletionProcessor#getIgnore()
+	 */
+	protected char[] getIgnore() {
+		return IGNORE;
 	}
 
-	protected Image getImage(Template template) {
-		return null;
+	/*
+	 * @see org.eclipse.dltk.ui.templates.ScriptTemplateCompletionProcessor#getTemplateAccess()
+	 */
+	protected ScriptTemplateAccess getTemplateAccess() {
+		return TclTemplateAccess.getInstance();
 	}
 }
