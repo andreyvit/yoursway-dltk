f6893753522a54badda4f8c5c404bf24c505ab39
ssanders <ssanders>
2008-03-27 04:05:47 +0000

    Corrected length assertion.

--- ruby/tests/org.eclipse.dltk.ruby.core.tests/src/org/eclipse/dltk/ruby/core/tests/search/mixin/MixinModelManipulationTests.java
+++ ruby/tests/org.eclipse.dltk.ruby.core.tests/src/org/eclipse/dltk/ruby/core/tests/search/mixin/MixinModelManipulationTests.java
@@ -113,7 +113,7 @@ public class MixinModelManipulationTests extends AbstractDLTKSearchTests impleme
 		assertNotNull(mixinElement);
 		Object[] objs = mixinElement.getAllObjects();
 		assertNotNull(objs);
-		assertEquals(1, objs.length);
+		assertEquals(2, objs.length);
 
 		ISourceModule sourceModule = getSourceModule(PROJECT_NAME, "src",
 				"src1.rb");
