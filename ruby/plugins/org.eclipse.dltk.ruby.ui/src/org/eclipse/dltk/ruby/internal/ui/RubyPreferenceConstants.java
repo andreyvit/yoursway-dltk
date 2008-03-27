b1d85ecbabed62f617221eca1dc6150dcec26ecd
ssanders <ssanders>
2008-03-27 03:54:23 +0000

    Changed default for FORMATTER_TAB_CHAR to SPACE.

--- ruby/plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/RubyPreferenceConstants.java
+++ ruby/plugins/org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/RubyPreferenceConstants.java
@@ -402,7 +402,7 @@ public class RubyPreferenceConstants extends PreferenceConstants
 //		store.setDefault(PreferenceConstants.EDITOR_FOLDING_METHODS, false);
 //		store.setDefault(PreferenceConstants.EDITOR_FOLDING_IMPORTS, true);
 		
-		store.setDefault (CodeFormatterConstants.FORMATTER_TAB_CHAR, CodeFormatterConstants.TAB);
+		store.setDefault (CodeFormatterConstants.FORMATTER_TAB_CHAR, CodeFormatterConstants.SPACE);
 		store.setDefault (CodeFormatterConstants.FORMATTER_TAB_SIZE, "2"); //$NON-NLS-1$
 		store.setDefault (CodeFormatterConstants.FORMATTER_INDENTATION_SIZE, "2"); //$NON-NLS-1$
 		
